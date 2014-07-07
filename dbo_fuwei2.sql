5daa34dd0119ab4a28bdf12cafa2585300207a6a
emilyyufen <emilyyufen@gmail.com>
2014-07-07 18:09:19 +0800

    Signed-off-by: emilyyufen <emilyyufen@gmail.com>

---
+++ dbo_fuwei2.sql
@@ -0,0 +1,308 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: dbo_fuwei2
+Target Host: localhost
+Target Database: dbo_fuwei2
+Date: 2014/7/7 18:08:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for companyprice
+-- ----------------------------
+DROP TABLE IF EXISTS `companyprice`;
+CREATE TABLE `companyprice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyId` int(11) NOT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `productName` varchar(255) DEFAULT NULL,
+  `salesManId` int(11) NOT NULL,
+  `sampleId` int(11) NOT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for developer
+-- ----------------------------
+DROP TABLE IF EXISTS `developer`;
+CREATE TABLE `developer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jianChen` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for fworder
+-- ----------------------------
+DROP TABLE IF EXISTS `fworder`;
+CREATE TABLE `fworder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyId` int(11) NOT NULL,
+  `companyOrderNumber` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `end_at` datetime DEFAULT NULL,
+  `fwOrderNumber` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `quoteId` int(11) NOT NULL,
+  `salesmanId` int(11) NOT NULL,
+  `start_at` datetime DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `status` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for orderstatus
+-- ----------------------------
+DROP TABLE IF EXISTS `orderstatus`;
+CREATE TABLE `orderstatus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `descript` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `orderId` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sampleprice
+-- ----------------------------
+DROP TABLE IF EXISTS `sampleprice`;
+CREATE TABLE `sampleprice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `memo` varchar(255) DEFAULT NULL,
+  `orderId` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sampleId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_company
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_company`;
+CREATE TABLE `tb_company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `companyNumber` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `help_code` varchar(255) DEFAULT NULL,
+  `level` int(11) NOT NULL,
+  `shortname` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK38B0A3AC87AC0D3A` (`created_user`),
+  CONSTRAINT `FK38B0A3AC87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_gongxu
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_gongxu`;
+CREATE TABLE `tb_gongxu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4AB8624F87AC0D3A` (`created_user`),
+  CONSTRAINT `FK4AB8624F87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_module
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_module`;
+CREATE TABLE `tb_module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `decription` varchar(255) DEFAULT NULL,
+  `inUse` bit(1) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK54F11D5D87AC0D3A` (`created_user`),
+  CONSTRAINT `FK54F11D5D87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_quote
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_quote`;
+CREATE TABLE `tb_quote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `has_quoted` bit(1) DEFAULT NULL,
+  `has_sure` bit(1) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `salesmanId` int(11) NOT NULL,
+  `sampleId` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKFAB6A56B87AC0D3A` (`created_user`),
+  CONSTRAINT `FKFAB6A56B87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_quoteorder
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_quoteorder`;
+CREATE TABLE `tb_quoteorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `excelUrl` varchar(255) DEFAULT NULL,
+  `quotationNumber` varchar(255) DEFAULT NULL,
+  `quote_ids` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK93908CC387AC0D3A` (`created_user`),
+  CONSTRAINT `FK93908CC387AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_role
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_role`;
+CREATE TABLE `tb_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `decription` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKA4FDFFA787AC0D3A` (`created_user`),
+  CONSTRAINT `FKA4FDFFA787AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_role_module
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_role_module`;
+CREATE TABLE `tb_role_module` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_user` int(11) DEFAULT NULL,
+  `moduleId` int(11) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKA5B50A049710D9E3` (`roleId`),
+  KEY `FKA5B50A0487AC0D3A` (`created_user`),
+  KEY `FKA5B50A04F98C994F` (`moduleId`),
+  CONSTRAINT `FKA5B50A04F98C994F` FOREIGN KEY (`moduleId`) REFERENCES `tb_module` (`id`),
+  CONSTRAINT `FKA5B50A0487AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`),
+  CONSTRAINT `FKA5B50A049710D9E3` FOREIGN KEY (`roleId`) REFERENCES `tb_role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_salesman
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_salesman`;
+CREATE TABLE `tb_salesman` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `help_code` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `companyId` int(11) DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7497AF7F2FD17BF9` (`companyId`),
+  KEY `FK7497AF7F87AC0D3A` (`created_user`),
+  CONSTRAINT `FK7497AF7F87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`),
+  CONSTRAINT `FK7497AF7F2FD17BF9` FOREIGN KEY (`companyId`) REFERENCES `tb_company` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_sample
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_sample`;
+CREATE TABLE `tb_sample` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cost` double NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL,
+  `has_detail` bit(1) DEFAULT NULL,
+  `help_code` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `machine` varchar(255) DEFAULT NULL,
+  `material` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `productNumber` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `weight` double NOT NULL,
+  `charge_user` int(11) DEFAULT NULL,
+  `created_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5E6CEC9B87AC0D3A` (`created_user`),
+  KEY `FK5E6CEC9B4D2DE8EE` (`charge_user`),
+  CONSTRAINT `FK5E6CEC9B4D2DE8EE` FOREIGN KEY (`charge_user`) REFERENCES `tb_user` (`id`),
+  CONSTRAINT `FK5E6CEC9B87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `help_code` varchar(255) DEFAULT NULL,
+  `inUse` bit(1) DEFAULT NULL,
+  `locked` bit(1) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKA4FF6AFC9710D9E3` (`roleId`),
+  CONSTRAINT `FKA4FF6AFC9710D9E3` FOREIGN KEY (`roleId`) REFERENCES `tb_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for unpricedsample
+-- ----------------------------
+DROP TABLE IF EXISTS `unpricedsample`;
+CREATE TABLE `unpricedsample` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `developerId` int(11) NOT NULL,
+  `machine` varchar(255) DEFAULT NULL,
+  `material` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `picturePath` varchar(255) DEFAULT NULL,
+  `productNumber` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `weight` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `tb_gongxu` VALUES ('1', '2014-07-07 17:18:04', 'v', '2014-07-07 17:18:04', '1');
+INSERT INTO `tb_gongxu` VALUES ('2', '2014-07-07 17:22:01', 'v', '2014-07-07 17:22:01', '1');
+INSERT INTO `tb_role` VALUES ('1', '2014-07-07 17:15:33', null, 'name', '2014-07-07 17:15:43', '1');
+INSERT INTO `tb_user` VALUES ('1', null, null, null, '', '', '余芬', '123456', null, null, null, 'yf', '1');
