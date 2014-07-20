3ad5c67bac5a778499ba2c1ce50c41ca95324307
hupan <emilyyufen@gmail.com>
2014-07-20 16:11:09 +0800

    Signed-off-by: hupan <emilyyufen@gmail.com>

--- dbo_fuwei.sql
+++ dbo_fuwei.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: dbo_fuwei
 Target Host: localhost
 Target Database: dbo_fuwei
-Date: 2014/7/19 13:21:05
+Date: 2014/7/20 16:10:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -151,7 +151,7 @@ CREATE TABLE `tb_company` (
   PRIMARY KEY (`id`),
   KEY `FK38B0A3AC87AC0D3A` (`created_user`),
   CONSTRAINT `FK38B0A3AC87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_gongxu
@@ -202,7 +202,7 @@ CREATE TABLE `tb_quote` (
   CONSTRAINT `FKFAB6A56B87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`),
   CONSTRAINT `FKFAB6A56B87AC0D4A` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`),
   CONSTRAINT `FKFAB6A56B88AC0D3A` FOREIGN KEY (`quotePriceId`) REFERENCES `tb_quoteprice` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_quoteorder
@@ -221,7 +221,7 @@ CREATE TABLE `tb_quoteorder` (
   KEY `FK3423222` (`salesmanId`),
   CONSTRAINT `FK3423222` FOREIGN KEY (`salesmanId`) REFERENCES `tb_salesman` (`id`),
   CONSTRAINT `FK93908CC387AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_quoteorder_detail
@@ -251,7 +251,7 @@ CREATE TABLE `tb_quoteorder_detail` (
   KEY `quoteOrderId` (`quoteOrderId`),
   CONSTRAINT `FKSample2` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`),
   CONSTRAINT `tb_quoteorder_detail_ibfk_3` FOREIGN KEY (`quoteOrderId`) REFERENCES `tb_quoteorder` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_quoteprice
@@ -270,7 +270,7 @@ CREATE TABLE `tb_quoteprice` (
   PRIMARY KEY (`id`),
   KEY `FKFAB6A56B87AC0D4A` (`created_user`),
   CONSTRAINT `FKFAB6A56B87AC0D5A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -355,7 +355,7 @@ CREATE TABLE `tb_sample` (
   KEY `FK5E6CEC9B4D2DE8EE` (`charge_user`),
   CONSTRAINT `FK5E6CEC9B4D2DE8EE` FOREIGN KEY (`charge_user`) REFERENCES `tb_user` (`id`),
   CONSTRAINT `FK5E6CEC9B87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_user
@@ -403,13 +403,12 @@ CREATE TABLE `unpricedsample` (
 -- ----------------------------
 INSERT INTO `tb_company` VALUES ('1', '公司地址', '杭州市', null, null, '2014-07-10 19:46:36', '公司', 'gs', '0', '公司', '2014-07-10 19:46:36', '1');
 INSERT INTO `tb_company` VALUES ('2', '西湖区文一西路243号', '杭州市', null, null, '2014-07-13 20:22:16', '测试公司', 'csgs', '0', '公司2', '2014-07-13 20:22:16', '1');
+INSERT INTO `tb_company` VALUES ('3', '西湖区文一西路998号', '杭州市', null, null, '2014-07-20 11:24:03', '测试公司2', 'csgs', '0', '测试', '2014-07-20 11:24:03', '1');
 INSERT INTO `tb_gongxu` VALUES ('1', '2014-07-07 17:18:04', '测试工序', '2014-07-12 22:34:52', '1');
 INSERT INTO `tb_gongxu` VALUES ('2', '2014-07-07 17:22:01', '测试工序2', '2014-07-12 22:35:07', '1');
 INSERT INTO `tb_gongxu` VALUES ('3', '2014-07-07 19:53:12', '测试工序3', '2014-07-12 22:35:16', '1');
 INSERT INTO `tb_gongxu` VALUES ('4', '2014-07-08 21:01:53', '测试', '2014-07-08 21:01:53', '1');
-INSERT INTO `tb_quote` VALUES ('12', '2014-07-19 00:33:21', '2014-07-19 00:33:21', '1', '5', '16');
-INSERT INTO `tb_quote` VALUES ('13', '2014-07-19 12:52:00', '2014-07-19 12:52:00', '1', '6', '19');
-INSERT INTO `tb_quote` VALUES ('14', '2014-07-19 12:52:03', '2014-07-19 12:52:03', '1', '6', '19');
+INSERT INTO `tb_quote` VALUES ('20', '2014-07-20 15:09:06', '2014-07-20 15:09:06', '1', '9', '22');
 INSERT INTO `tb_quoteorder` VALUES ('1', '2014-07-18 17:48:06', null, 'FWA10001', '2014-07-18 17:48:06', '1', '1');
 INSERT INTO `tb_quoteorder` VALUES ('2', '2014-07-18 22:49:06', 'upload/excel/20140718224906_1405694946935.xls', 'FWA10002', '2014-07-18 22:49:06', '1', '2');
 INSERT INTO `tb_quoteorder` VALUES ('3', '2014-07-18 23:05:19', 'upload/excel/20140718230519_1405695919044.xls', 'FWA10003', '2014-07-18 23:05:19', '1', '2');
@@ -422,6 +421,11 @@ INSERT INTO `tb_quoteorder` VALUES ('10', '2014-07-19 00:55:35', 'upload/excel/2
 INSERT INTO `tb_quoteorder` VALUES ('11', '2014-07-19 00:57:20', 'upload/excel/20140719005720_1405702640062.xls', 'FWA10011', '2014-07-19 00:57:20', '1', '1');
 INSERT INTO `tb_quoteorder` VALUES ('12', '2014-07-19 01:01:37', 'upload/excel/20140719010137_1405702897967.xls', 'FWA10012', '2014-07-19 01:01:37', '1', '1');
 INSERT INTO `tb_quoteorder` VALUES ('13', '2014-07-19 01:08:08', 'upload/excel/20140719010808_1405703288531.xls', 'FWA10013', '2014-07-19 01:08:08', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('14', '2014-07-20 11:48:17', 'upload/excel/20140720114817_1405828097498.xls', 'FWA10014', '2014-07-20 11:48:17', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('15', '2014-07-20 14:57:07', 'upload/excel/20140720145707_1405839427523.xls', 'FWA10015', '2014-07-20 14:57:07', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('16', '2014-07-20 15:02:36', 'upload/excel/20140720150236_1405839756929.xls', 'FWA10016', '2014-07-20 15:02:36', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('17', '2014-07-20 15:10:29', 'upload/excel/20140720151028_1405840228772.xls', 'FWA10017', '2014-07-20 15:10:29', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('18', '2014-07-20 15:11:46', 'upload/excel/20140720151145_1405840305531.xls', 'FWA10018', '2014-07-20 15:11:46', '1', '1');
 INSERT INTO `tb_quoteorder_detail` VALUES ('1', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload\\1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '1', '3', '新建公司报价备注', '34', null, null, null);
 INSERT INTO `tb_quoteorder_detail` VALUES ('2', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload\\1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '2', '3', '备注', '15', 'kuhao2', null, null);
 INSERT INTO `tb_quoteorder_detail` VALUES ('3', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload\\1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '3', '3', '备注', '15', 'kuhao2', null, null);
@@ -429,17 +433,16 @@ INSERT INTO `tb_quoteorder_detail` VALUES ('4', '3.967', '材料1：1*1*12*2÷ 1
 INSERT INTO `tb_quoteorder_detail` VALUES ('5', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload\\1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '5', '3', 'hgh', '54', 'kuhao1', null, null);
 INSERT INTO `tb_quoteorder_detail` VALUES ('6', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload\\1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '6', '3', 'hgh', '54', 'kuhao1', null, null);
 INSERT INTO `tb_quoteorder_detail` VALUES ('7', '3.967', '材料1：1*1*12*2÷ 1000=0.024\r\n材料2：1*3*12*4÷ 1000=0.144\r\n0.024+0.144=0.168+1=1.168\r\n0.036+0.18=0.216\r\n测试工序3:6.000\r\n测试工序2:8.000\r\n测试工序:9.000\r\n____________________________\r\n23+0.216\r\n=23.216*2\r\n=46.432+1.168\r\n=47.6÷12\r\n=3.967*3\r\n=11.901\r\n', 'upload/images/1404742143164DSC00104.JPG', '1', '1', '1', '1', '1', '1', '1', '7', '3', '备注', '15', 'kuhao2', null, null);
-INSERT INTO `tb_quoteorder_detail` VALUES ('8', '8.769', '34*1*12*3÷ 1000=1.224+2=3.224\r\n\r\n测试工序:34.000\r\n____________________________\r\n34+0\r\n=34*3\r\n=102+3.224\r\n=105.224÷12\r\n=8.769*4\r\n=35.076\r\n', 'upload/images/140570077131920140716_230651.jpg', '他', '复古', '功夫', '自动生成', '听个歌', '234', '1', '8', '16', 'gg', '23', '44', null, null);
-INSERT INTO `tb_quoteorder_detail` VALUES ('10', '15.597', '345*22*12*2÷ 1000=182.16+1=183.16\r\n\r\n测试工序:4.000\r\n____________________________\r\n4+0\r\n=4*1\r\n=4+183.16\r\n=187.16÷12\r\n=15.597*1\r\n=15.597\r\n', 'upload/images/140570235895420140716_230719.jpg', '机织', '材料', '测试', '自动生成', '尺寸', '233', '1', '10', '19', 'fff', '56', 'kuanhao', 'upload/images/s/140570235895420140716_230719.jpg', 'upload/images/ss/140570235895420140716_230719.png');
-INSERT INTO `tb_quoteorder_detail` VALUES ('11', '15.597', '345*22*12*2÷ 1000=182.16+1=183.16\r\n\r\n测试工序:4.000\r\n____________________________\r\n4+0\r\n=4*1\r\n=4+183.16\r\n=187.16÷12\r\n=15.597*1\r\n=15.597\r\n', 'upload/images/140570235895420140716_230719.jpg', '机织', '材料', '测试', '自动生成', '尺寸', '233', '1', '11', '19', 'fff', '56', 'kuanhao', 'upload/images/s/140570235895420140716_230719.jpg', 'upload/images/ss/140570235895420140716_230719.png');
-INSERT INTO `tb_quoteorder_detail` VALUES ('12', '15.597', '345*22*12*2÷ 1000=182.16+1=183.16\r\n\r\n测试工序:4.000\r\n____________________________\r\n4+0\r\n=4*1\r\n=4+183.16\r\n=187.16÷12\r\n=15.597*1\r\n=15.597\r\n', 'upload/images/140570235895420140716_230719.jpg', '机织', '材料', '测试', '自动生成', '尺寸', '233', '1', '12', '19', 'fff', '56', 'kuanhao', 'upload/images/s/140570235895420140716_230719.jpg', 'upload/images/ss/140570235895420140716_230719.png');
-INSERT INTO `tb_quoteorder_detail` VALUES ('13', '15.597', '345*22*12*2÷ 1000=182.16+1=183.16\r\n\r\n测试工序:4.000\r\n____________________________\r\n4+0\r\n=4*1\r\n=4+183.16\r\n=187.16÷12\r\n=15.597*1\r\n=15.597\r\n', 'upload/images/140570235895420140716_230719.jpg', '机织', '材料', '测试', '自动生成', '尺寸', '233', '1', '13', '19', 'fff', '56', 'kuanhao', 'upload/images/s/140570235895420140716_230719.jpg', 'upload/images/ss/140570235895420140716_230719.png');
+INSERT INTO `tb_quoteorder_detail` VALUES ('15', '26.961', '45*1.1*12*12÷ 1000=7.128+3=10.128\r\n0.772\r\n测试工序:44.000\r\n____________________________\r\n44+0.772\r\n=44.772*7\r\n=313.404+10.128\r\n=323.532÷12\r\n=26.961*1.3\r\n=35.049\r\n', 'upload/images/1405837478092KK119 KK123.jpg', '机织', '材料', '测试款号', 'FWA30022', '尺寸', '4', '1', '15', '22', '', '33', 'kk', 'upload/images/s/1405837478092KK119 KK123.jpg', 'upload/images/ss/1405837478092KK119 KK123.png');
+INSERT INTO `tb_quoteorder_detail` VALUES ('16', '26.961', '45*1.1*12*12÷ 1000=7.128+3=10.128\r\n0.772\r\n测试工序:44.000\r\n____________________________\r\n44+0.772\r\n=44.772*7\r\n=313.404+10.128\r\n=323.532÷12\r\n=26.961*1.3\r\n=35.049\r\n', 'upload/images/1405837478092KK119 KK123.jpg', '机织', '材料', '测试款号', 'FWA30022', '尺寸', '4', '1', '16', '22', 'fff', '4', 'kuanhao', 'upload/images/s/1405837478092KK119 KK123.jpg', 'upload/images/ss/1405837478092KK119 KK123.png');
+INSERT INTO `tb_quoteorder_detail` VALUES ('17', '26.961', '45*1.1*12*12÷ 1000=7.128+3=10.128\r\n0.772\r\n测试工序:44.000\r\n____________________________\r\n44+0.772\r\n=44.772*7\r\n=313.404+10.128\r\n=323.532÷12\r\n=26.961*1.3\r\n=35.049\r\n', 'upload/images/1405837478092KK119 KK123.jpg', '机织', '材料', '测试款号', 'FWA30022', '尺寸', '4', '1', '17', '22', '', '33', 'kk', 'upload/images/s/1405837478092KK119 KK123.jpg', 'upload/images/ss/1405837478092KK119 KK123.png');
+INSERT INTO `tb_quoteorder_detail` VALUES ('18', '26.961', '45*1.1*12*12÷ 1000=7.128+3=10.128\r\n0.772\r\n测试工序:44.000\r\n____________________________\r\n44+0.772\r\n=44.772*7\r\n=313.404+10.128\r\n=323.532÷12\r\n=26.961*1.3\r\n=35.049\r\n', 'upload/images/1405837478092KK119 KK123.jpg', '机织', '材料', '测试款号', 'FWA30022', '尺寸', '4', '1', '18', '22', 'fff', '4', 'kuanhao', 'upload/images/s/1405837478092KK119 KK123.jpg', 'upload/images/ss/1405837478092KK119 KK123.png');
 INSERT INTO `tb_quoteprice` VALUES ('1', '2014-07-13 20:08:52', 'hgh', '54', '1', '3', '2014-07-18 17:19:34', '1', 'kuhao1');
 INSERT INTO `tb_quoteprice` VALUES ('2', '2014-07-13 20:25:38', '新建公司报价备注', '34', '1', '3', '2014-07-15 23:32:02', '1', null);
 INSERT INTO `tb_quoteprice` VALUES ('3', '2014-07-13 20:25:55', '备注', '15', '2', '3', '2014-07-18 17:19:23', '1', 'kuhao2');
 INSERT INTO `tb_quoteprice` VALUES ('4', '2014-07-15 23:15:22', 'gfd', '50', '1', '3', '2014-07-15 23:31:25', '1', null);
-INSERT INTO `tb_quoteprice` VALUES ('5', '2014-07-19 00:33:14', 'gg', '23', '1', '16', '2014-07-19 00:33:14', '1', '44');
-INSERT INTO `tb_quoteprice` VALUES ('6', '2014-07-19 00:53:43', 'fff', '56', '1', '19', '2014-07-19 00:53:43', '1', 'kuanhao');
+INSERT INTO `tb_quoteprice` VALUES ('8', '2014-07-20 14:55:48', 'fff', '4', '1', '22', '2014-07-20 14:55:48', '1', 'kuanhao');
+INSERT INTO `tb_quoteprice` VALUES ('9', '2014-07-20 14:56:01', '', '33', '1', '22', '2014-07-20 14:56:01', '1', 'kk');
 INSERT INTO `tb_role` VALUES ('1', '2014-07-07 17:15:33', null, 'name', '2014-07-07 17:15:43', '1');
 INSERT INTO `tb_salesman` VALUES ('1', '2014-07-10 19:46:49', 'ywy', '业务员', '212455', '2014-07-10 19:46:49', '1', '1');
 INSERT INTO `tb_salesman` VALUES ('2', '2014-07-13 20:22:27', 'csywy', '测试业务员2', '15033747932', '2014-07-13 20:22:37', '2', '1');
@@ -451,11 +454,7 @@ INSERT INTO `tb_sample` VALUES ('7', '1', '2014-07-07 22:46:12', null, '', '', '
 INSERT INTO `tb_sample` VALUES ('8', '100', '2014-07-14 22:16:53', null, '', 'csypm', 'upload/images/1405347413660loading.gif', '机织', '材料', '备注3', '测试样品名3', '3', '尺寸', '2014-07-14 22:16:53', '344', '1', '1', null, null);
 INSERT INTO `tb_sample` VALUES ('9', '30', '2014-07-14 22:17:26', null, '', 'csypm', 'upload/images/1405347446340QQ图片20140313221759.jpg', '机织', '材料', '备注4', '测试样品名4', '4', '尺寸', '2014-07-14 22:17:26', '45', '1', '1', null, null);
 INSERT INTO `tb_sample` VALUES ('10', '24', '2014-07-14 22:20:03', null, '', 'csypm', 'upload/images/1405347603880QQ图片20140313221759.jpg', '机织', '材料', '55', '测试样品名5', '5', '344', '2014-07-14 22:20:03', '45', '1', '1', null, null);
-INSERT INTO `tb_sample` VALUES ('11', '0', '2014-07-18 22:11:54', null, '', 'csyp', 'upload/images/1405692713960QQ图片20140313221759.jpg', '机织', '棉麻', '备注', '测试样品2', '自动生成', '100尺', '2014-07-18 22:11:54', '100', '1', '1', null, null);
-INSERT INTO `tb_sample` VALUES ('14', '0', '2014-07-18 22:40:31', null, '', '', 'upload/images/140569443168820140716_230634.jpg', '2', '1', '4', '15068821518', '自动生成', '1', '2014-07-18 22:40:31', '1', '1', '1', null, null);
-INSERT INTO `tb_sample` VALUES ('15', '0', '2014-07-19 00:24:41', null, '', 'tt', 'upload/images/140570060278420140716_230634.jpg', '广告歌', '发', '孤云野鹤', '45天天', '自动生成', '热', '2014-07-19 00:24:41', '542', '1', '1', 'upload/images/s/140570060278420140716_230634.jpg', 'upload/images/ss/140570060278420140716_230634.jpg');
-INSERT INTO `tb_sample` VALUES ('16', '8.769', '2014-07-19 00:26:13', '34*1*12*3÷ 1000=1.224+2=3.224\r\n\r\n测试工序:34.000\r\n____________________________\r\n34+0\r\n=34*3\r\n=102+3.224\r\n=105.224÷12\r\n=8.769*4\r\n=35.076\r\n', '', 'gf', 'upload/images/140570077131920140716_230651.jpg', '他', '复古', '让人好', '功夫', '自动生成', '听个歌', '2014-07-19 00:26:13', '234', '1', '1', 'upload/images/s/140570077131920140716_230651.jpg', 'upload/images/ss/140570077131920140716_230651.jpg');
-INSERT INTO `tb_sample` VALUES ('18', '0', '2014-07-19 00:50:45', null, '', 'cs', 'upload/images/140570224298420140716_230719.jpg', '机织', '材料', '备注', '测试', '自动生成', '尺寸', '2014-07-19 00:50:45', '233', '1', '1', 'upload/images/s/140570224298420140716_230719.jpg', 'upload/images/ss/140570224298420140716_230719.png');
-INSERT INTO `tb_sample` VALUES ('19', '15.597', '2014-07-19 00:52:41', '345*22*12*2÷ 1000=182.16+1=183.16\r\n\r\n测试工序:4.000\r\n____________________________\r\n4+0\r\n=4*1\r\n=4+183.16\r\n=187.16÷12\r\n=15.597*1\r\n=15.597\r\n', '', 'cs', 'upload/images/140570235895420140716_230719.jpg', '机织', '材料', '备注', '测试', '自动生成', '尺寸', '2014-07-19 00:52:41', '233', '1', '1', 'upload/images/s/140570235895420140716_230719.jpg', 'upload/images/ss/140570235895420140716_230719.png');
+INSERT INTO `tb_sample` VALUES ('22', '26.961', '2014-07-20 14:24:38', '45*1.1*12*12÷ 1000=7.128+3=10.128\r\n0.772\r\n测试工序:44.000\r\n____________________________\r\n44+0.772\r\n=44.772*7\r\n=313.404+10.128\r\n=323.532÷12\r\n=26.961*1.3\r\n=35.049\r\n', '', 'cskh', 'upload/images/1405837478092KK119 KK123.jpg', '机织', '材料', '', '测试款号', 'FWA30022', '尺寸', '2014-07-20 14:25:40', '4', '1', '1', 'upload/images/s/1405837478092KK119 KK123.jpg', 'upload/images/ss/1405837478092KK119 KK123.png');
+INSERT INTO `tb_sample` VALUES ('23', '0', '2014-07-20 15:42:39', null, '', '', 'upload/images/1405842153342IMG_7725.JPG', '3 ', 'f', '', 'f', 'FWA30023', '发', '2014-07-20 15:42:39', '3', '1', '1', 'upload/images/s/1405842153342IMG_7725.JPG', 'upload/images/ss/1405842153342IMG_7725.png');
 INSERT INTO `tb_user` VALUES ('1', null, '6@qq.com', 'yf', '', '', '余芬', '123456', '123', '143333', '2014-07-07 19:54:08', 'yf', '1');
 INSERT INTO `tb_user` VALUES ('2', '2014-07-10 20:07:03', '345@qq.com', 'yf', '', '', '余芬2', '123456', '12335', '124577', '2014-07-10 20:07:03', 'yufen', '1');
