294b126763c2c7cc0718d23689f4ede58ba68509
emilyyufen <emilyyufen@gmail.com>
2014-07-23 18:06:15 +0800

    添加权限表
    Signed-off-by: emilyyufen <emilyyufen@gmail.com>

--- dbo_fuwei.sql
+++ dbo_fuwei.sql
@@ -4,10 +4,37 @@ Source Host: localhost
 Source Database: dbo_fuwei
 Target Host: localhost
 Target Database: dbo_fuwei
-Date: 2014/7/22 0:14:47
+Date: 2014/7/23 18:05:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for companyprice
+-- ----------------------------
+DROP TABLE IF EXISTS `companyprice`;
+CREATE TABLE `companyprice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyId` int(11) NOT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `productName` varchar(255) DEFAULT NULL,
+  `salesManId` int(11) NOT NULL,
+  `sampleId` int(11) NOT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for developer
+-- ----------------------------
+DROP TABLE IF EXISTS `developer`;
+CREATE TABLE `developer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jianChen` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- ----------------------------
 -- Table structure for fworder
 -- ----------------------------
@@ -45,6 +72,80 @@ CREATE TABLE `orderstatus` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for quotation
+-- ----------------------------
+DROP TABLE IF EXISTS `quotation`;
+CREATE TABLE `quotation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyPriceID` int(11) NOT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for quotationlist
+-- ----------------------------
+DROP TABLE IF EXISTS `quotationlist`;
+CREATE TABLE `quotationlist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyName` varchar(255) DEFAULT NULL,
+  `excelName` varchar(255) DEFAULT NULL,
+  `idString` varchar(255) DEFAULT NULL,
+  `quotationNumber` varchar(255) DEFAULT NULL,
+  `salesName` varchar(255) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sample
+-- ----------------------------
+DROP TABLE IF EXISTS `sample`;
+CREATE TABLE `sample` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cost` double NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL,
+  `developerId` int(11) NOT NULL,
+  `machine` varchar(255) DEFAULT NULL,
+  `material` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `picturePath` varchar(255) DEFAULT NULL,
+  `productNumber` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `weight` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sampleprice
+-- ----------------------------
+DROP TABLE IF EXISTS `sampleprice`;
+CREATE TABLE `sampleprice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `memo` varchar(255) DEFAULT NULL,
+  `orderId` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sampleId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_authority`;
+CREATE TABLE `tb_authority` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `cname` varchar(255) DEFAULT NULL,
+  `lcode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKPID323` (`pid`),
+  CONSTRAINT `FKPID323` FOREIGN KEY (`pid`) REFERENCES `tb_authority` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for tb_company
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_company`;
@@ -64,7 +165,7 @@ CREATE TABLE `tb_company` (
   PRIMARY KEY (`id`),
   KEY `FK38B0A3AC87AC0D3A` (`created_user`),
   CONSTRAINT `FK38B0A3AC87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_gongxu
@@ -134,7 +235,7 @@ CREATE TABLE `tb_quoteorder` (
   KEY `FK3423222` (`salesmanId`),
   CONSTRAINT `FK3423222` FOREIGN KEY (`salesmanId`) REFERENCES `tb_salesman` (`id`),
   CONSTRAINT `FK93908CC387AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_quoteorder_detail
@@ -162,9 +263,9 @@ CREATE TABLE `tb_quoteorder_detail` (
   PRIMARY KEY (`id`),
   KEY `FKSample2` (`sampleId`),
   KEY `quoteOrderId` (`quoteOrderId`),
-  CONSTRAINT `tb_quoteorder_detail_ibfk_4` FOREIGN KEY (`quoteOrderId`) REFERENCES `tb_quoteorder` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FKSample2` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `FKSample2` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`),
+  CONSTRAINT `tb_quoteorder_detail_ibfk_4` FOREIGN KEY (`quoteOrderId`) REFERENCES `tb_quoteorder` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_quoteprice
@@ -183,9 +284,9 @@ CREATE TABLE `tb_quoteprice` (
   PRIMARY KEY (`id`),
   KEY `FKFAB6A56B87AC0D4A` (`created_user`),
   KEY `FKS` (`sampleId`),
-  CONSTRAINT `FKS` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`),
-  CONSTRAINT `FKFAB6A56B87AC0D5A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  CONSTRAINT `FKFAB6A56B87AC0D5A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`),
+  CONSTRAINT `FKS` FOREIGN KEY (`sampleId`) REFERENCES `tb_sample` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_role
@@ -202,6 +303,24 @@ CREATE TABLE `tb_role` (
   KEY `FKA4FDFFA787AC0D3A` (`created_user`),
   CONSTRAINT `FKA4FDFFA787AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tb_role_authority
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_role_authority`;
+CREATE TABLE `tb_role_authority` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_user` int(11) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  `authorityId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `authorityId` (`authorityId`),
+  KEY `created_user` (`created_user`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `tb_role_authority_ibfk_3` FOREIGN KEY (`roleId`) REFERENCES `tb_role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tb_role_authority_ibfk_1` FOREIGN KEY (`authorityId`) REFERENCES `tb_authority` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tb_role_authority_ibfk_2` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_role_module
@@ -270,7 +389,7 @@ CREATE TABLE `tb_sample` (
   KEY `FK5E6CEC9B4D2DE8EE` (`charge_user`),
   CONSTRAINT `FK5E6CEC9B4D2DE8EE` FOREIGN KEY (`charge_user`) REFERENCES `tb_user` (`id`),
   CONSTRAINT `FK5E6CEC9B87AC0D3A` FOREIGN KEY (`created_user`) REFERENCES `tb_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for tb_user
@@ -296,6 +415,24 @@ CREATE TABLE `tb_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for unpricedsample
+-- ----------------------------
+DROP TABLE IF EXISTS `unpricedsample`;
+CREATE TABLE `unpricedsample` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `developerId` int(11) NOT NULL,
+  `machine` varchar(255) DEFAULT NULL,
+  `material` varchar(255) DEFAULT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `picturePath` varchar(255) DEFAULT NULL,
+  `productNumber` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `weight` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Procedure structure for truncate
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `truncate`;
@@ -307,6 +444,53 @@ DELIMITER ;
 -- ----------------------------
 -- Records 
 -- ----------------------------
+INSERT INTO `tb_authority` VALUES ('1', null, '权限管理', 'authority');
+INSERT INTO `tb_authority` VALUES ('2', '1', '查看权限列表', 'authority/index');
+INSERT INTO `tb_authority` VALUES ('3', '1', '修改用户权限', 'authority/update');
+INSERT INTO `tb_authority` VALUES ('4', null, '系统信息管理', 'systeminfo/index');
+INSERT INTO `tb_authority` VALUES ('5', '4', '用户管理', 'user');
+INSERT INTO `tb_authority` VALUES ('6', '5', '查看用户列表', 'user/index');
+INSERT INTO `tb_authority` VALUES ('7', '5', '添加用户权限', 'user/add');
+INSERT INTO `tb_authority` VALUES ('8', '5', '编辑用户权限', 'user/edit');
+INSERT INTO `tb_authority` VALUES ('9', '5', '删除用户权限', 'user/delete');
+INSERT INTO `tb_authority` VALUES ('10', '4', '公司管理', 'company');
+INSERT INTO `tb_authority` VALUES ('11', '10', '查看公司列表', 'company/index');
+INSERT INTO `tb_authority` VALUES ('12', '10', '添加公司', 'company/add');
+INSERT INTO `tb_authority` VALUES ('13', '10', '编辑公司', 'company/edit');
+INSERT INTO `tb_authority` VALUES ('14', '10', '删除公司', 'company/delete');
+INSERT INTO `tb_authority` VALUES ('15', '4', '业务员管理', 'salesman');
+INSERT INTO `tb_authority` VALUES ('16', '15', '查看业务员列表', 'salesman/index');
+INSERT INTO `tb_authority` VALUES ('17', '15', '添加业务员', 'salesman/add');
+INSERT INTO `tb_authority` VALUES ('18', '15', '编辑业务员', 'salesman/edit');
+INSERT INTO `tb_authority` VALUES ('19', '15', '删除业务员', 'salesman/delete');
+INSERT INTO `tb_authority` VALUES ('20', '4', '工序管理', 'gongxu');
+INSERT INTO `tb_authority` VALUES ('21', '20', '查看工序列表', 'gongxu/index');
+INSERT INTO `tb_authority` VALUES ('22', '20', '添加工序', 'gongxu/add');
+INSERT INTO `tb_authority` VALUES ('23', '20', '编辑工序', 'gongxu/edit');
+INSERT INTO `tb_authority` VALUES ('24', '20', '删除工序', 'gongxu/delete');
+INSERT INTO `tb_authority` VALUES ('25', null, '样品管理', 'sample');
+INSERT INTO `tb_authority` VALUES ('26', '25', '查看样品列表', 'sample/index');
+INSERT INTO `tb_authority` VALUES ('27', '25', '未核价列表', 'sample/undetailedindex');
+INSERT INTO `tb_authority` VALUES ('28', '25', '添加样品', 'sample/add');
+INSERT INTO `tb_authority` VALUES ('29', '25', '编辑样品', 'sample/edit');
+INSERT INTO `tb_authority` VALUES ('30', '25', '删除样品', 'sample/delete');
+INSERT INTO `tb_authority` VALUES ('31', '25', '查看样品详情', 'sample/detail');
+INSERT INTO `tb_authority` VALUES ('32', '25', '生成样品标签', 'sample/print_sign');
+INSERT INTO `tb_authority` VALUES ('33', '25', '打印样品详情', 'sample/print_detail');
+INSERT INTO `tb_authority` VALUES ('34', '25', '样品核价', 'sample/set_detail');
+INSERT INTO `tb_authority` VALUES ('35', '25', '公司价格管理', 'quoteprice');
+INSERT INTO `tb_authority` VALUES ('36', '35', '查看价格列表', 'quoteprice/index');
+INSERT INTO `tb_authority` VALUES ('37', '35', '添加公司价格', 'quoteprice/add');
+INSERT INTO `tb_authority` VALUES ('38', '35', '编辑公司价格', 'quoteprice/edit');
+INSERT INTO `tb_authority` VALUES ('39', '35', '删除公司价格', 'quoteprice/delete');
+INSERT INTO `tb_authority` VALUES ('40', '35', '打印价格详情', 'quoteprice/print');
+INSERT INTO `tb_authority` VALUES ('41', '25', '报价管理', 'quote');
+INSERT INTO `tb_authority` VALUES ('42', '41', '添加报价', 'quote/add');
+INSERT INTO `tb_authority` VALUES ('43', '41', '删除报价', 'quote/delete');
+INSERT INTO `tb_authority` VALUES ('44', '25', '报价单管理', 'quoteorder');
+INSERT INTO `tb_authority` VALUES ('45', '44', '查看报价单列表', 'quoteorder/index');
+INSERT INTO `tb_authority` VALUES ('46', '44', '生成报价单', 'quoteorder/add');
+INSERT INTO `tb_authority` VALUES ('47', '44', '查看报价单详情', 'quoteorder/detail');
 INSERT INTO `tb_company` VALUES ('1', '公司地址', '杭州市', null, null, '2014-07-10 19:46:36', '公司', 'gs', '0', '公司', '2014-07-10 19:46:36', '1');
 INSERT INTO `tb_company` VALUES ('2', '西湖区文一西路243号', '杭州市', null, null, '2014-07-13 20:22:16', '测试公司', 'csgs', '0', '公司2', '2014-07-13 20:22:16', '1');
 INSERT INTO `tb_company` VALUES ('3', '西湖区文一西路998号', '杭州市', null, null, '2014-07-20 11:24:03', '测试公司2', 'csgs', '0', '测试', '2014-07-20 11:24:03', '1');
@@ -315,13 +499,21 @@ INSERT INTO `tb_gongxu` VALUES ('2', '2014-07-07 17:22:01', '测试工序2', '20
 INSERT INTO `tb_gongxu` VALUES ('3', '2014-07-07 19:53:12', '测试工序3', '2014-07-12 22:35:16', '1');
 INSERT INTO `tb_gongxu` VALUES ('4', '2014-07-08 21:01:53', '测试', '2014-07-08 21:01:53', '1');
 INSERT INTO `tb_quote` VALUES ('7', '2014-07-21 22:56:04', '2014-07-21 22:56:04', '1', '1', '2');
+INSERT INTO `tb_quote` VALUES ('8', '2014-07-23 16:23:03', '2014-07-23 16:23:03', '1', '3', '3');
 INSERT INTO `tb_quoteorder` VALUES ('1', '2014-07-21 22:56:54', 'resource.fuwei.com/excel/quote/1_报价单20140721225654.xls', 'FWA10001', '2014-07-21 22:56:54', '1', '1');
+INSERT INTO `tb_quoteorder` VALUES ('2', '2014-07-22 12:48:43', 'resource.fuwei.com/excel/quote/2_报价单20140722124843.xls', 'FWA10002', '2014-07-22 12:48:43', '1', '1');
 INSERT INTO `tb_quoteorder_detail` VALUES ('1', '2.576', '100*1.1*12*10÷ 1000=13.2+10=23.2\r\n1.716\r\n测试工序:6.000\r\n____________________________\r\n6+1.716\r\n=7.716*1\r\n=7.716+23.2\r\n=30.916÷12\r\n=2.576*1.3\r\n=3.349\r\n', 'resource.fuwei.com/images/sample/140595351558420140716_230658.jpg', '机织', '材料', '测试', 'FWA30002', '尺寸', '100', '2', '1', '2', '', '40', 'AS1', 'resource.fuwei.com/images/sample/s/140595351558420140716_230658.jpg', 'resource.fuwei.com/images/sample/ss/140595351558420140716_230658.png');
+INSERT INTO `tb_quoteorder_detail` VALUES ('2', '3.057', '55*1.1*12*10÷ 1000=7.26+10=17.26\r\n0.944\r\n测试工序:6.000\r\n测试工序2:8.000\r\n____________________________\r\n14+0.944\r\n=14.944*1.3\r\n=19.427+17.26\r\n=36.687÷12\r\n=3.057*1.3\r\n=3.974\r\n', 'resource.fuwei.com/images/sample/1406002140620QQ图片20140717133813.jpg', '机织', '材料', '测试样品', 'FWA30003', '尺寸', '55', '1', '2', '3', '', '50', 'S3F', 'resource.fuwei.com/images/sample/s/1406002140620QQ图片20140717133813.png', 'resource.fuwei.com/images/sample/ss/1406002140620QQ图片20140717133813.png');
 INSERT INTO `tb_quoteprice` VALUES ('1', '2014-07-21 22:56:01', '', '40', '1', '2', '2014-07-21 22:56:01', '1', 'AS1');
 INSERT INTO `tb_quoteprice` VALUES ('2', '2014-07-21 22:56:11', '', '50', '1', '2', '2014-07-21 22:56:11', '1', 'AS4');
-INSERT INTO `tb_role` VALUES ('1', '2014-07-07 17:15:33', null, 'name', '2014-07-07 17:15:43', '1');
+INSERT INTO `tb_quoteprice` VALUES ('3', '2014-07-22 12:16:19', '', '50', '1', '3', '2014-07-22 12:16:19', '1', 'S3F');
+INSERT INTO `tb_role` VALUES ('1', '2014-07-07 17:15:33', '系统管理员', 'admin', '2014-07-07 17:15:43', '1');
+INSERT INTO `tb_role_authority` VALUES ('1', '1', '1', '1');
+INSERT INTO `tb_role_authority` VALUES ('2', '1', '1', '2');
+INSERT INTO `tb_role_authority` VALUES ('3', '1', '1', '3');
 INSERT INTO `tb_salesman` VALUES ('1', '2014-07-10 19:46:49', 'ywy', '业务员', '212455', '2014-07-10 19:46:49', '1', '1');
 INSERT INTO `tb_salesman` VALUES ('2', '2014-07-13 20:22:27', 'csywy', '测试业务员2', '15033747932', '2014-07-13 20:22:37', '2', '1');
 INSERT INTO `tb_sample` VALUES ('2', '2.576', '2014-07-21 22:38:38', '100*1.1*12*10÷ 1000=13.2+10=23.2\r\n1.716\r\n测试工序:6.000\r\n____________________________\r\n6+1.716\r\n=7.716*1\r\n=7.716+23.2\r\n=30.916÷12\r\n=2.576*1.3\r\n=3.349\r\n', '', 'cs', 'resource.fuwei.com/images/sample/140595351558420140716_230658.jpg', '机织', '材料', '', '测试', 'FWA30002', '尺寸', '2014-07-21 22:53:03', '100', '2', '1', 'resource.fuwei.com/images/sample/s/140595351558420140716_230658.png', 'resource.fuwei.com/images/sample/ss/140595351558420140716_230658.png');
+INSERT INTO `tb_sample` VALUES ('3', '3.057', '2014-07-22 12:09:01', '55*1.1*12*10÷ 1000=7.26+10=17.26\r\n0.944\r\n测试工序:6.000\r\n测试工序2:8.000\r\n____________________________\r\n14+0.944\r\n=14.944*1.3\r\n=19.427+17.26\r\n=36.687÷12\r\n=3.057*1.3\r\n=3.974\r\n', '', 'csyp', 'resource.fuwei.com/images/sample/1406002140620QQ图片20140717133813.jpg', '机织', '材料', '', '测试样品', 'FWA30003', '尺寸', '2014-07-22 12:09:01', '55', '1', '1', 'resource.fuwei.com/images/sample/s/1406002140620QQ图片20140717133813.png', 'resource.fuwei.com/images/sample/ss/1406002140620QQ图片20140717133813.png');
 INSERT INTO `tb_user` VALUES ('1', null, '6@qq.com', 'yf', '', '', '余芬', '1', '123', '143333', '2014-07-21 11:46:25', 'yf', '1');
 INSERT INTO `tb_user` VALUES ('2', '2014-07-10 20:07:03', '345@qq.com', 'yf', '', '', '余芬2', '123456', '12335', '124577', '2014-07-10 20:07:03', 'yufen', '1');
